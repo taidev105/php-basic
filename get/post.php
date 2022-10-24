b2546f90d281cf29744cf85dd57ae4457bf4797f
Trần Mạnh Tài <101389458+taidev105@users.noreply.github.com>
2022-10-25 06:05:10 +0700

    Update post.php

--- get/post.php
+++ get/post.php
@@ -2,3 +2,9 @@
 echo 'Tên: ' . $_POST['name'] . '<br>';
 echo 'Tuổi: ' . $_POST['age'];
 ?>
+
+<?php
+if (isset($_POST['name']) && isset($_POST['age'])) {
+    echo 'Tên: ' . $_POST['name'] . '<br>';
+    echo 'Tuổi: ' . $_POST['age'];
+}
